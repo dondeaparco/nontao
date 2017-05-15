1cc9d6742e9e6e26a3e43b419ac09c99e95fb178
dondeaparco <nontao.vit@gmail.com>
2017-05-15 12:22:21 +0200

    Add files via upload

---
+++ kafka-test2.py
@@ -0,0 +1,21 @@
+import sys
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+sc = SparkContext(appName="kafkaTest")
+#sc.setLogLevel("WARN")  
+ssc = StreamingContext(sc,20)
+
+
+kvs = KafkaUtils.createStream(ssc, "sandbox.hortonworks.com:2181", "spark_streaming", {"test": 1})
+kvs.map(lambda x : x[1]).pprint()
+
+
+
+ssc.start()
+
+ssc.awaitTermination()
+
+
